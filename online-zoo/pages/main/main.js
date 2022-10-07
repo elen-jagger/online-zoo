0c2a871e9ff19f77752cd9fea90e0f49d9fc32d4
Elena Leusik <elena.leusik@gmail.com>
2022-10-07 11:49:48 +0200

    feat: add popup for Testimonials section at Main page

--- online-zoo/pages/main/main.js
+++ online-zoo/pages/main/main.js
@@ -1,24 +1,58 @@
 //  Burger menu
 const btn = document.querySelector('.header__bgr-icon');
 const menu = document.querySelector('.menu');
-const shadow = document.querySelector('.header__shadow');
+const headerShadow = document.querySelector('.header__shadow');
 const html = document.querySelector('.html');
 const menuItem = document.querySelectorAll('.menu__item');
 
 function toggleMenu() {
   btn.classList.toggle("header__bgr-icon_open");               
   menu.classList.toggle("menu__open");  
-  shadow.classList.toggle("header__shadow_open");
+  headerShadow.classList.toggle("header__shadow_open");
   //html.classList.toggle("no-scroll");
 }
 
 function closeMenu() {
   btn.classList.remove("header__bgr-icon_open");               
   menu.classList.remove("menu__open");  
-  shadow.classList.remove("header__shadow_open");
+  headerShadow.classList.remove("header__shadow_open");
   //html.classList.remove("no-scroll");
 }
 
 btn.addEventListener('click', toggleMenu);
 menuItem.forEach((el) => el.addEventListener('click', closeMenu));
-shadow.addEventListener('click', closeMenu);
+headerShadow.addEventListener('click', closeMenu);
+
+// Popup
+const cards = document.querySelectorAll('.testimonials-card__gradient');
+const testimonialsShadow = document.querySelector('.testimonials__shadow');
+const popupCloseBtn = document.querySelectorAll('.popup__close-btn');
+
+function popupOpen(e) {
+  const card = e.currentTarget;
+  const popup = card.parentElement;
+
+  popup.classList.add("testimonials-popup", "testimonials-popup_open");
+  testimonialsShadow.classList.add('testimonials__shadow_open');
+}
+
+function popupClose(e) {
+  const btn = e.currentTarget;
+  const popup = btn.parentElement;
+  
+  popup.classList.remove('testimonials-popup_open', 'testimonials-popup');
+  testimonialsShadow.classList.remove('testimonials__shadow_open');
+  console.log('hi');
+}
+
+function shadowPopupClose() {
+  const openedPopup = document.querySelector('.testimonials-popup_open');
+  openedPopup.classList.remove('testimonials-popup_open', 'testimonials-popup');
+  testimonialsShadow.classList.remove('testimonials__shadow_open');
+}
+
+
+cards.forEach((el) => el.addEventListener('click', popupOpen));
+
+popupCloseBtn.forEach((el) => el.addEventListener('click', popupClose));
+testimonialsShadow.addEventListener('click', shadowPopupClose)
