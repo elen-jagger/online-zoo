b0ade9a72b0cf55c45851e2f1bb5956ef94470da
Elena Leusik <elena.leusik@gmail.com>
2022-10-09 21:40:06 +0200

    feat: add slider in Testimonials section at Main page

--- online-zoo/pages/main/main.js
+++ online-zoo/pages/main/main.js
@@ -9,53 +9,18 @@ function toggleMenu() {
   btn.classList.toggle("header__bgr-icon_open");               
   menu.classList.toggle("menu__open");  
   headerShadow.classList.toggle("header__shadow_open");
-  //html.classList.toggle("no-scroll");
 }
 
 function closeMenu() {
   btn.classList.remove("header__bgr-icon_open");               
   menu.classList.remove("menu__open");  
   headerShadow.classList.remove("header__shadow_open");
-  //html.classList.remove("no-scroll");
 }
 
 btn.addEventListener('click', toggleMenu);
 menuItem.forEach((el) => el.addEventListener('click', closeMenu));
 headerShadow.addEventListener('click', closeMenu);
 
-// Popup
-const cards = document.querySelectorAll('.testimonials-card__gradient');
-const testimonialsShadow = document.querySelector('.testimonials__shadow');
-const popupCloseBtn = document.querySelectorAll('.popup__close-btn');
-
-function popupOpen(e) {
-  const card = e.currentTarget;
-  const popup = card.parentElement;
-
-  popup.classList.add("testimonials-popup", "testimonials-popup_open");
-  testimonialsShadow.classList.add('testimonials__shadow_open');
-}
-
-function popupClose(e) {
-  const btn = e.currentTarget;
-  const popup = btn.parentElement;
-  
-  popup.classList.remove('testimonials-popup_open', 'testimonials-popup');
-  testimonialsShadow.classList.remove('testimonials__shadow_open');
-  console.log('hi');
-}
-
-function shadowPopupClose() {
-  const openedPopup = document.querySelector('.testimonials-popup_open');
-  openedPopup.classList.remove('testimonials-popup_open', 'testimonials-popup');
-  testimonialsShadow.classList.remove('testimonials__shadow_open');
-}
-
-
-cards.forEach((el) => el.addEventListener('click', popupOpen));
-
-popupCloseBtn.forEach((el) => el.addEventListener('click', popupClose));
-testimonialsShadow.addEventListener('click', shadowPopupClose)
 
 // Pets slider
 const leftBtn = document.querySelector('.pets__btn_left');
@@ -91,3 +56,73 @@ function showSlides(n) {
 
 leftBtn.addEventListener('click', () => plusSlides(-1));
 rightBtn.addEventListener('click', () => plusSlides(1));
+
+
+// Popup
+const cards = document.querySelectorAll('.testimonials-card__gradient');
+const testimonialsShadow = document.querySelector('.testimonials__shadow');
+const popupCloseBtn = document.querySelectorAll('.popup__close-btn');
+
+function popupOpen(e) {
+  const card = e.currentTarget;
+  const popup = card.parentElement;
+
+  popup.classList.add("testimonials-popup", "testimonials-popup_open");
+  testimonialsShadow.classList.add('testimonials__shadow_open');
+}
+
+function popupClose(e) {
+  const btn = e.currentTarget;
+  const popup = btn.parentElement;
+  
+  popup.classList.remove('testimonials-popup_open', 'testimonials-popup');
+  testimonialsShadow.classList.remove('testimonials__shadow_open');
+  console.log('hi');
+}
+
+function shadowPopupClose() {
+  const openedPopup = document.querySelector('.testimonials-popup_open');
+  openedPopup.classList.remove('testimonials-popup_open', 'testimonials-popup');
+  testimonialsShadow.classList.remove('testimonials__shadow_open');
+}
+
+
+cards.forEach((el) => el.addEventListener('click', popupOpen));
+
+popupCloseBtn.forEach((el) => el.addEventListener('click', popupClose));
+testimonialsShadow.addEventListener('click', shadowPopupClose)
+
+//Testimonials slider
+const carousel = document.querySelector('.testimonials__slider-container');
+const carouselContent = document.querySelector('.testimonials-cards-container');
+const input = document.querySelector('.testimonials__progress-bar');
+
+let initValue = 1;
+
+function moveCarousel() {
+  let width = cards[0].offsetWidth;
+  
+  let gap = 30;
+  let newValue = (input.value - initValue);
+  console.log(input.value);
+  console.log(newValue);
+  let scrollX;
+
+  scrollX = (width + gap) * newValue;
+  console.log(scrollX);
+  carousel.scrollBy(scrollX, 0);
+  initValue = input.value;
+}
+
+function startCarousel() {
+  input.removeEventListener('input', moveCarousel);
+}
+
+function endCarousel() {
+  moveCarousel();
+  input.addEventListener('input', moveCarousel);
+}
+
+input.addEventListener('input', moveCarousel);
+input.addEventListener('touchstart', startCarousel);
+input.addEventListener('touchend', endCarousel);
